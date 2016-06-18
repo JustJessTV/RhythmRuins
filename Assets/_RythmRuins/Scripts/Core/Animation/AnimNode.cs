498d4b781df81ee1d1a8b68e4b10541261c19695
Brandon Farrell <Brandon@BPFarrell.com>
2016-06-18 01:49:50 -0700

    Holy Crap Animations

---
+++ Assets/_RythmRuins/Scripts/Core/Animation/AnimNode.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimNode{
+    public AnimNodeState state=AnimNodeState.Idle;
+    public AnimNodeType type;
+    public AnimNode PreAnimation;
+    public AnimNode PostAnimation;
+    public System.Action OnComplete;
+    public SpriteRenderer sr;
+    Sprite[] frames;
+    float fps;
+    float spf;
+    float timeTIllNextFrame;
+    int index;
+    public AnimNode(ref SpriteRenderer sr, Sprite[] frames, int start, int end, float fps, AnimNodeType type) {
+        this.frames = SubSet(ref frames, start, end);
+        this.fps = fps;
+        spf = 1 / fps;
+        this.type=type;
+        this.sr = sr;
+    }
+    public void Update() {
+        switch (state) {
+            case AnimNodeState.Playing:
+                FrameCalculate();
+                break;
+            case AnimNodeState.WaitingPre:
+                if (PreAnimation != null) PreAnimation.Update();
+                break;
+            case AnimNodeState.WaitingPost:
+                if (PostAnimation != null) PostAnimation.Update();
+                break;
+
+        }
+    }
+    void FrameCalculate() {
+        if (timeTIllNextFrame < Time.time) {
+            timeTIllNextFrame += spf;
+            index++;
+            if (index >= frames.Length&&type==AnimNodeType.Single) {
+                TryComplete();
+                return;
+            }
+            sr.sprite = frames[index % frames.Length];
+        }
+    }
+    public void TryComplete() {
+        if (PostAnimation != null) {
+            state = AnimNodeState.WaitingPost;
+            PostAnimation.Play();
+            PostAnimation.OnComplete = SubAnimComplete;
+        }
+        else {
+            Finalize();
+        }
+    }
+    public void Play() {
+        if (state != AnimNodeState.Idle) return;
+        if (PreAnimation != null) {
+            PreAnimation.Play();
+            state = AnimNodeState.WaitingPre;
+            PreAnimation.OnComplete = SubAnimComplete;
+        }
+        else {
+            Start();
+        }
+    }
+    void Start() {
+        state = AnimNodeState.Playing;
+        index = 0;
+        timeTIllNextFrame = Time.time + spf;
+    }
+    void Finalize() {
+        state = AnimNodeState.Idle;
+        if (OnComplete != null)
+            OnComplete();
+    }
+    private void SubAnimComplete() {
+        if (state == AnimNodeState.WaitingPre) {
+            Start();
+        }
+        if (state == AnimNodeState.WaitingPost) {
+            Finalize();
+        }
+    }
+    Sprite[] SubSet(ref Sprite[] sprites, int start, int end) {
+        Sprite[] subSprite = new Sprite[end - start + 1];
+        for (int x = start; x < end + 1; x++) {
+            subSprite[x - start] = sprites[x];
+        }
+        return subSprite;
+    }
+}
---
+++ Assets/_RythmRuins/Scripts/Core/Animation/AnimTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimTest : MonoBehaviour {
+    SpriteRenderer sr;
+    Sprite[] sprites;
+    int index;
+    int fps = 14;
+    float spf;
+    float nextUpdate;
+    public bool finish = false;
+    AnimNode an;
+	// Use this for initialization
+	void Awake () {
+        sr = GetComponent<SpriteRenderer>();
+        sprites = Resources.LoadAll<Sprite>("MegaMan");
+        an = new AnimNode(ref sr, sprites, 19, 29, 12, AnimNodeType.Loop);
+        an.PreAnimation = new AnimNode(ref sr, sprites, 0, 9, 12, AnimNodeType.Single);
+        an.PostAnimation = new AnimNode(ref sr, sprites, 87, 90, 12, AnimNodeType.Single);
+        an.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        an.Update();
+        if (finish) {
+            finish = false;
+            an.TryComplete();
+        }
+	}
+}
