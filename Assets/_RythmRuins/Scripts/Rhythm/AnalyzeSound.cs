037984b8f3b1aff535ef708d6dd3578536f212eb
Vachara Nick Lertackakorn <vakara@hotmail.com>
2016-06-17 23:15:44 -0700

    returns float[] out min, max, avg

---
+++ Assets/_RythmRuins/Scripts/Rhythm/AnalyzeSound.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class AnalyzeSound : MonoBehaviour {
+
+    AudioMixer audMix;
+    AudioSource audSrc;
+    AudioClip audClp;
+
+    public float[] spectrum = new float[64];
+    public GameObject testGO;
+    public GameObject testGoLow;
+    public GameObject testGoMid;
+    // Use this for initialization
+    void Start () {
+        audSrc = GetComponent<AudioSource>();
+        audClp = audSrc.clip;
+	}
+
+    public static float[] GetSpectrum(AudioSource audSrc,int arraySize, out float max, out float avg, out float min) {
+        float[] spectrum = new float[arraySize];
+        audSrc.GetSpectrumData(spectrum, 0, FFTWindow.BlackmanHarris);
+        max = float.MinValue;
+        min = float.MaxValue;
+        avg = 0.0f;
+        float sum = 0.0f;
+        int size = spectrum.Length;
+        for (int j = 0; j < size; j++)
+        {
+            if (spectrum[j] > max) max = spectrum[j];
+            if (spectrum[j] < min &&
+                spectrum[j] > 0)
+            {
+                min = spectrum[j];
+            }
+            sum += spectrum[j];
+        }
+        avg = sum / size;
+        return spectrum;
+    }
+	
+}
+
