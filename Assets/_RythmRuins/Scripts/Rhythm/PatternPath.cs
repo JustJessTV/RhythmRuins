78f62f839c2cf0c315e8e9110214fe14f3f23ab1
Vachara Nick Lertackakorn <vakara@hotmail.com>
2016-06-18 18:44:37 -0700

    path manager for save/ load rhythm patterns

---
+++ Assets/_RythmRuins/Scripts/Rhythm/PatternPath.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using SimpleJSON;
+
+public class PatternPath : MonoBehaviour {
+
+    private static string _directoryPath;
+    public static string DIRECTORYPATH{
+        get {
+            return _directoryPath ?? (_directoryPath = Application.dataPath + "/" +"PatternFiles/"); 
+        }
+        set {
+            _directoryPath = value;
+        }
+    }
+    public static string FILENAME{
+        get{return "_DANCEPATTERN";}
+    }
+    private static List<string> _fileNames;
+    public static List<string> fileNames {
+        get {
+            if (_fileNames == null) {
+                _fileNames = new List<string>();
+            
+                var info = new DirectoryInfo(DIRECTORYPATH);
+                var fileInfo = info.GetFiles();
+                foreach (FileInfo f in fileInfo) {
+                    _fileNames.Add(f.ToString());
+                }
+            }
+            return _fileNames;
+        }
+    }
+    public static void SaveNewFile(JSONNode jnode, string name) {
+        if (!Directory.Exists(DIRECTORYPATH)) {
+            Directory.CreateDirectory(DIRECTORYPATH);
+        }
+        string stringData = jnode.ToString();
+        string prefix = fileNames.Count.ToString()+"_";
+        File.WriteAllText(DIRECTORYPATH + name + FILENAME, stringData);
+    }
+    public static string LoadPatternFile(string name) {
+        string result = "";
+        foreach (string s in fileNames) {
+            if (s.Contains(name) &&
+                !s.Contains(".meta")) {
+                Debug.Log("found :" + s);
+                result = File.ReadAllText(s);
+            }
+        }
+        return result;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
