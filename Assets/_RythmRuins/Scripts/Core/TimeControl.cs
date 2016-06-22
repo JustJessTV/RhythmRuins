c78b0a2cc4db8556d58af32a6f10b02fbd867162
Vachara Nick Lertackakorn <vakara@hotmail.com>
2016-06-21 18:31:41 -0700

    Time control for pausing game providing callbacks to pause / play sound track

---
+++ Assets/_RythmRuins/Scripts/Core/TimeControl.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeControl : MonoBehaviour {
+
+    public delegate void PauseGameEvent();
+    public static event PauseGameEvent pauseGameCallBack;
+
+    public delegate void UnPauseGameEvent();
+    public static event UnPauseGameEvent unPauseGameCallBack;
+
+    public static bool isPaused;
+
+    private static float unscaledPauseTime;
+    
+    private static float _timeScale;
+    private static float timeScale {
+        get {
+            return _timeScale;
+        }
+        set {
+            _timeScale = value;
+        }
+    }
+    public static void SetTimeScale(float t) {
+        timeScale = t;
+        Time.timeScale = timeScale;
+    }
+
+    // Please call this to Pause game;
+    public static void PauseGame() {
+        isPaused = true;
+        unscaledPauseTime = Time.unscaledTime;
+        Time.timeScale = 0;
+        if (pauseGameCallBack != null) pauseGameCallBack();
+    }
+
+    // Call this to unPause;
+    public static void UnPauseGame() {
+        isPaused = false;
+        Time.timeScale = timeScale;
+        if (unPauseGameCallBack != null) unPauseGameCallBack();
+    }
+
+    public static float TotalPausedTime {
+        get{
+            return Time.unscaledTime - unscaledPauseTime;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
