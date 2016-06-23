6906572d9f297291ea768bd4c0c7fa2b2a40e078
Vachara Nick Lertackakorn <vakara@hotmail.com>
2016-06-23 09:48:23 -0700

    game state handler

---
+++ Assets/_RythmRuins/Scripts/Core/GameStateHandler.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameStateHandler : MonoBehaviour {
+
+    public enum State {
+        transitionToMain,
+        main,
+        transitionToPlay,
+        beginGamePlay,
+        gamePlaying,
+        gameEnd,
+        gameOver
+    }
+
+    public static State gameState;
+    public string displayGameState;
+
+    public delegate void TransitionToMain();
+    public static event TransitionToMain transitionToMain;
+
+    public delegate void TransitionToPlay();
+    public static event TransitionToMain transitionToPlay;
+
+    public delegate void BeginPlay();
+    public static event BeginPlay beginPlay;
+
+    public delegate void PlayReady();
+    public static event PlayReady playReady;
+
+    public delegate void GameOver();
+    public static event GameOver gameOver;
+
+    public static void BeginGame(){
+        if (transitionToPlay != null) {
+            transitionToPlay();
+        }
+        gameState = State.transitionToPlay;
+    }
+    public static void EndGame() {
+        gameState = State.gameEnd;
+    }
+    public static void TransitionToPlayComplete() {
+        gameState = State.beginGamePlay;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        displayGameState = gameState.ToString();
+        if (gameState == State.transitionToMain) {
+            Debug.Log("call back from : " + gameState.ToString());
+            if (transitionToMain != null)
+            {
+                
+                transitionToMain();
+            }
+            gameState = State.main;
+        }
+        if (gameState == State.main)
+        {
+            
+        }
+        
+        if (gameState == State.transitionToPlay)
+        {
+            
+        }
+
+        if (gameState == State.beginGamePlay) {
+            Debug.Log("call back from : " + gameState.ToString());
+            if (playReady != null) {
+                playReady();
+            }
+            
+            gameState = State.gamePlaying;
+        }
+
+        if (gameState == State.gamePlaying)
+        {
+          
+        }
+
+        if (gameState == State.gameEnd)
+        {
+            Debug.Log("call back from : " + gameState.ToString());
+            if (gameOver != null)
+            {
+                gameOver();
+            }
+            gameState = State.gameOver;
+        }
+	}
+}
