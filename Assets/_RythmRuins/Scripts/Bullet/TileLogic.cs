e46a4da336d78cb1389517d3a5fbd80a27fb78c9
Brandon Farrell <Brandon@BPFarrell.com>
2016-06-23 13:30:52 -0700

    updated prefabs, and texture

--- Assets/_RythmRuins/Scripts/Bullet/TileLogic.cs
+++ Assets/_RythmRuins/Scripts/Bullet/TileLogic.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class TileLogic : MonoBehaviour {
     Renderer renderer;
+    public float width;
     bool hasSpawned = false;
     private Plane[] planes;
 	void Awake () {
@@ -14,9 +15,9 @@ public class TileLogic : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (!hasSpawned && CamController.GetDistFromRightWall(transform.position) < 20) {
+        if (!hasSpawned && CamController.GetDistFromRightWall(transform.position) < width) {
             hasSpawned = true;
-            GameObject go = Instantiate(gameObject,transform.position+Vector3.right*3,transform.rotation)as GameObject;
+            GameObject go = Instantiate(gameObject,transform.position+Vector3.right*width,transform.rotation)as GameObject;
         }
 	}
 }
