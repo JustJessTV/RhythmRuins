92af6bc6d735936e2d6caecd86b620ef8164c8a9
Vachara Nick Lertackakorn <vakara@hotmail.com>
2016-06-23 07:31:00 -0700

    set cam to getter

--- Assets/_RythmRuins/Scripts/Core/CamController.cs
+++ Assets/_RythmRuins/Scripts/Core/CamController.cs
@@ -5,11 +5,20 @@ public class CamController : MonoBehaviour {
     public static Vector3 LeftWall;
     public static Vector3 RightWall;
     public float speed = 2;
-    static Camera cam;
+    static Camera _cam;
+    static Camera cam {
+        get {
+            return _cam ?? (_cam = Camera.main.GetComponent<Camera>());
+        }
+        set {
+            _cam = value;
+        }
+    }
     float lastXSpawn = 0;
 	// Use this for initialization
     void Awake () {
         cam = GetComponent<Camera>();
+        Debug.Log(cam);
         Plane[] planes = GeometryUtility.CalculateFrustumPlanes(cam);
         LeftWall = Vector3.Cross(planes[0].normal, Vector3.up);
         RightWall = Vector3.Cross(planes[1].normal*-1, Vector3.up);
@@ -26,6 +35,8 @@ public class CamController : MonoBehaviour {
         transform.position += Vector3.right * Time.deltaTime*speed;
 	}
     public static Vector3 GetPointAtDistRight(float dist) {
+   //     Debug.Log(cam);
+    //    Debug.Log(RightWall);
         return cam.transform.position + RightWall * dist;
     }
     public static Vector3 GetPointAtDistLeft(float dist) {
