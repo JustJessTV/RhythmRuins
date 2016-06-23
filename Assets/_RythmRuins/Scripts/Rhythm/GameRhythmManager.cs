d308d9751c10d0c3fc1f4682edbadff474f2a880
Vachara Nick Lertackakorn <vakara@hotmail.com>
2016-06-23 02:21:04 -0700

    change to 4 but control

--- Assets/_RythmRuins/Scripts/Rhythm/GameRhythmManager.cs
+++ Assets/_RythmRuins/Scripts/Rhythm/GameRhythmManager.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace RhythmRealm
 {
     public class GameRhythmManager : MonoBehaviour
     {
-        public GameObject spawnL0, spawnL1, spawnL2, spawnR0, spawnR1, spawnR2;
-        public GameObject endL0, endL1, endL2, endR0, endR1, endR2;
+        public static List<GameObject> spawnNodes  = new List<GameObject>();//spawn_L0, spawn_L1, spawn_L2, spawn_R0, spawn_R1, spawn_R2;
+        public static List<GameObject> endNodes    = new List<GameObject>();//end_L0, end_L1, end_L2, end_R0, end_R1, end_R2;
 
         public bool setPathNodes;
 
@@ -24,15 +25,45 @@ namespace RhythmRealm
         // Use this for initialization
         void Start()
         {
-
-            PatternBehave.SpawnPath pathL0 = new PatternBehave.SpawnPath(spawnL0.transform.position, endL0.transform.position, 0);
-            PatternBehave.SpawnPath pathL1 = new PatternBehave.SpawnPath(spawnL1.transform.position, endL1.transform.position, 1);
-            PatternBehave.SpawnPath pathL2 = new PatternBehave.SpawnPath(spawnL2.transform.position, endL2.transform.position, 2);
-            PatternBehave.SpawnPath pathR0 = new PatternBehave.SpawnPath(spawnR0.transform.position, endR0.transform.position, 3);
-            PatternBehave.SpawnPath pathR1 = new PatternBehave.SpawnPath(spawnR1.transform.position, endR1.transform.position, 4);
-            PatternBehave.SpawnPath pathR2 = new PatternBehave.SpawnPath(spawnR2.transform.position, endR2.transform.position, 5);
+            
+            CreateSpawnNodes();
+            PatternBehave.SpawnPath pathL0 = new PatternBehave.SpawnPath(spawnNodes[0], endNodes[0], 0);
+            PatternBehave.SpawnPath pathL1 = new PatternBehave.SpawnPath(spawnNodes[1], endNodes[1], 1);
+            PatternBehave.SpawnPath pathR0 = new PatternBehave.SpawnPath(spawnNodes[2], endNodes[2], 2);
+            PatternBehave.SpawnPath pathR1 = new PatternBehave.SpawnPath(spawnNodes[3], endNodes[3], 3);
+   //         PatternBehave.SpawnPath pathR1 = new PatternBehave.SpawnPath(spawn_R1.transform.position, end_R1.transform.position, 4);
+   //         PatternBehave.SpawnPath pathR2 = new PatternBehave.SpawnPath(spawn_R2.transform.position, end_R2.transform.position, 5);
         }
+        void CreateSpawnNodes() {
 
+            GameObject[] nodesPrefabs = Resources.LoadAll<GameObject>("Beats/SpawnNodes");// as GameObject[];
+            List<GameObject> nodesObjects = new List<GameObject>();
+            for (int n = 0; n < nodesPrefabs.Length; n++){ // (GameObject n in nodesPrefabs) {
+                
+                if (nodesPrefabs[n].name.Contains("2")) continue;
+                GameObject go = Instantiate(nodesPrefabs[n]);
+                nodesObjects.Add(go);
+            }
+            for (int i = 0; i < nodesObjects.Count; i++)
+            {
+                if (nodesObjects[i].name.Contains("L2") ||
+                    nodesObjects[i].name.Contains("R2"))
+                {
+                    Debug.Log("Cancel!");
+                    continue; // cancel last button;
+                }
+                if (nodesObjects[i].name.Contains("spawn"))
+                {
+                    Debug.Log("add spawn :" + nodesObjects[i].name);
+                    spawnNodes.Add(nodesObjects[i]);
+                }
+                if (nodesObjects[i].name.Contains("end"))
+                {
+                    Debug.Log("add end :" + nodesObjects[i].name);
+                    endNodes.Add(nodesObjects[i]);
+                }
+            }
+        }
         // Update is called once per frame
         void Update()
         {
